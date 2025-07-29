d4009b6de2b5532703a384aa7f8bf5dba89e1a57
Saikat-Raj <saikat.raj.cs@gmail.com>
2025-07-29 20:50:31 -0300

    refactored

--- group3-wiot-final.ino
+++ group3-wiot-final.ino
@@ -1,10 +1,377 @@
 
 #include <constants.h>
 #include <secrets.h>
-#include <wifiDataSender.h>
-#include <storeData.h>
-#include <bluetoothScanner.h>
-#include <utils.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <time.h>
+#include <SPIFFS.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+// Constants for WiFi data sender
+#define RETRY_COUNTER 3
+#define ACK_TIMEOUT 5000 // 5 seconds
+
+// WifiDataSender class
+class WifiDataSender {
+private:
+    const char* _ssid;
+    const char* _password;
+    const char* _udpAddress;
+    unsigned int _udpPort;
+    WiFiUDP _udp;
+    bool _packetAcknowledged;
+    uint8_t _retryCounter;
+    bool _debug;
+
+    void _connectToWiFi() {
+        WiFi.mode(WIFI_STA);
+        DEBUG_LOGN("-- LOG: Connecting to WiFi...");
+        WiFi.begin(_ssid, _password);
+
+        unsigned long timeout = millis() + 10000; // 10 second timeout
+
+        while (WiFi.status() != WL_CONNECTED && millis() < timeout) {
+            delay(100);
+            DEBUG_LOG(".");
+        }
+
+        if (WiFi.status() != WL_CONNECTED) {
+            DEBUG_LOGN("\n-- ERROR: WiFi Connection Failed!!");
+            return;
+        }
+
+        DEBUG_LOGN("\n-- SUCCESS: WiFi Connected!!");
+        DEBUG_LOG("-- LOG: IP Address: ");
+        DEBUG_LOGN(WiFi.localIP());
+
+        _udp.begin(_udpPort);
+    }
+
+    bool waitForAcknowledgment() {
+        unsigned long timeout = millis() + ACK_TIMEOUT;
+        
+        while (millis() < timeout) {
+            int packetSize = _udp.parsePacket();
+            if (packetSize > 0) {
+                char ackBuffer[4];
+                int len = _udp.read(ackBuffer, sizeof(ackBuffer) - 1);
+                if (len > 0) {
+                    ackBuffer[len] = '\0';
+                    if (strcmp(ackBuffer, "ACK") == 0) {
+                        return true;
+                    }
+                }
+            }
+            delay(10);
+        }
+        return false;
+    }
+
+    bool _sendDataWithConfirmation(const char* data) {
+        for (_retryCounter = 1; _retryCounter <= RETRY_COUNTER; _retryCounter++) {
+            // Send UDP packet
+            _udp.beginPacket(_udpAddress, _udpPort);
+            _udp.print(data);
+            _udp.endPacket();
+
+            if (_debug) {
+                DEBUG_LOG("-- LOG: Sent packet #");
+                DEBUG_LOGN(_retryCounter);
+            }
+
+            // Wait for acknowledgment
+            if (waitForAcknowledgment()) {
+                DEBUG_LOGN("-- SUCCESS: ACK Received!!");
+                return true;
+            }
+            
+            DEBUG_LOGN("-- LOG: ACK timeout, retrying...");
+        }
+
+        DEBUG_LOGN("-- ERROR: Max retries exceeded!!");
+        return false;
+    }
+
+public:
+    WifiDataSender(const char* ssid, const char* password, const char* udpAddress, unsigned int udpPort, bool debug)
+        : _ssid(ssid), _password(password), _udpAddress(udpAddress), _udpPort(udpPort),
+          _packetAcknowledged(false), _debug(debug), _retryCounter(0) {}
+
+    unsigned long getUnixTime() {
+        if (WiFi.status() != WL_CONNECTED) {
+            _connectToWiFi();
+        }
+
+        configTime(0, 0, TIME_SERVER);
+
+        struct tm timeinfo;
+        if (!getLocalTime(&timeinfo)) {
+            return 0;
+        }
+        
+        time_t now;
+        time(&now);
+        return now;
+    }
+
+    bool uploadData(const String data) {
+        if (WiFi.status() != WL_CONNECTED) {
+            _connectToWiFi();
+        }
+
+        return _sendDataWithConfirmation(data.c_str());
+    }
+};
+
+// Data storage functions
+void storeData(const char* fileName, const String data) {
+    DEBUG_LOGF("-- LOG: Writing file: %s\r\n", fileName);
+    File file = SPIFFS.open(fileName, FILE_APPEND);
+    if (!file) {
+        DEBUG_LOGN("-- ERROR: Failed to open file!");
+        return;
+    }
+
+    if (file && file.size() == 0) {
+        DEBUG_LOGN("-- LOG: Created the Data File!!");
+        file.println("timeStamp,peerId,rssi,deviceId,uploadDuration,contactDuration,closeContactDuration,exposureStatus");
+    }
+
+    if (!file.println(data)) {
+        DEBUG_LOGN("-- ERROR: Write failed!");
+    }
+
+    file.close();
+}
+
+String readData(const char* fileName) {
+    if (!SPIFFS.exists(fileName)) {
+        DEBUG_LOGN("-- ERROR: File doesn't exist!");
+        return String();
+    }
+
+    File file = SPIFFS.open(fileName, "r");
+    if (!file) {
+        DEBUG_LOGN("-- ERROR: Failed to open file!");
+        return String();
+    }
+
+    String content = "";
+    while (file.available()) {
+        content += (char)file.read();
+    }
+    DEBUG_LOGN("-- LOG: Data:");
+    DEBUG_LOGN(content);
+
+    file.close();
+    return content;
+}
+
+// Utility functions
+void checkSPIFFS() {
+    DEBUG_LOGN("\n--- SPIFFS Diagnostics ---");
+ 
+    if (!SPIFFS.begin(true)) {
+        DEBUG_LOGN("-- ERROR: SPIFFS Mount Failed! Formatting...");
+        SPIFFS.format();
+        if (!SPIFFS.begin(true)) {
+            DEBUG_LOGN("-- ERROR: Format Failed! Halting.");
+            while(1);
+        }
+    }
+
+    DEBUG_LOGF("-- LOG: Total space: %10d bytes\n", SPIFFS.totalBytes());
+    DEBUG_LOGF("-- LOG: Used space:  %10d bytes\n", SPIFFS.usedBytes());
+
+    DEBUG_LOGN("\n-- LOG: File List:");
+    File root = SPIFFS.open("/");
+    File file = root.openNextFile();
+    while(file){
+        DEBUG_LOGF("-- LOG:   %-20s %8d bytes\n", file.name(), file.size());
+        file = root.openNextFile();
+    }
+
+    const char* testFile = "./spiffs_test.txt";
+    DEBUG_LOGF("\n-- LOG: Testing write to %s...", testFile);
+
+    File test = SPIFFS.open(testFile, FILE_WRITE);
+    if(test) {
+        test.println("-- SUCCESS: SPIFFS TEST SUCCESS");
+        test.close();
+        DEBUG_LOGN("OK");
+
+        test = SPIFFS.open(testFile, FILE_READ);
+        DEBUG_LOG("-- LOG: Contents: ");
+        while(test.available()) Serial.write(test.read());
+        test.close();
+    } else {
+        DEBUG_LOGN("-- ERROR: FAILED");
+    }
+
+    SPIFFS.remove(testFile);
+    DEBUG_LOGN("--- End Diagnostics ---\n");
+}
+
+// BluetoothScanner class
+class BluetoothScanner {
+private:
+    const unsigned long startTime;
+    String deviceId;
+    unsigned long lastUploadDuration;
+
+    String generateDeviceId() {
+        String id = "ESP32_";
+        for (int i = 0; i < DEVICE_ID_LENGTH; i++) {
+            id += String(random(0, 10));
+        }
+        return id;
+    }
+
+    void updateDeviceId() {
+        deviceId = generateDeviceId();
+        DEBUG_LOG("Updated Device ID: ");
+        DEBUG_LOGN(deviceId);
+        
+        BLEAdvertising *advertising = BLEDevice::getAdvertising();
+        advertising->stop();
+        
+        BLEAdvertisementData adData;
+        adData.setManufacturerData(deviceId);
+        advertising->setAdvertisementData(adData);
+        
+        advertising->start();
+    }
+
+    bool isContactTracingDevice(BLEAdvertisedDevice device) {
+        String manufacturerData = device.getManufacturerData();
+
+        if (manufacturerData.length() > 0) {
+            if (manufacturerData.indexOf("ESP32_") != -1 && manufacturerData != deviceId) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void recordDeviceContact(BLEAdvertisedDevice device) {
+        String deviceAddress = device.getAddress().toString();
+        int rssi = device.getRSSI();
+        
+        unsigned long currentTime = startTime + (millis() / 1000);
+        
+        unsigned long firstSeen = getFirstSeenTime(deviceAddress.c_str());
+        if (firstSeen == 0) {
+            addOrUpdateTrackedDevice(deviceAddress.c_str(), currentTime);
+            firstSeen = currentTime;
+            DEBUG_LOG("First contact with device: ");
+            DEBUG_LOG(deviceAddress);
+            DEBUG_LOG(" at time: ");
+            DEBUG_LOGN(currentTime);
+        }
+        
+        updateCloseContact(deviceAddress.c_str(), currentTime, rssi);
+        
+        unsigned long contactDuration = currentTime - firstSeen;
+        unsigned long closeContactDuration = getCloseContactDuration(deviceAddress.c_str());
+        
+        int index = findTrackedDevice(deviceAddress.c_str());
+        if (index >= 0 && lastCloseContactTimes[index] > 0) {
+            closeContactDuration += (currentTime - lastCloseContactTimes[index]);
+        }
+        
+        bool isExposure = isExposureEvent(deviceAddress.c_str(), currentTime);
+        String exposureStatus = isExposure ? "EXPOSURE" : "NORMAL";
+        
+        DEBUG_LOG("Device: ");
+        DEBUG_LOG(deviceAddress);
+        DEBUG_LOG(" RSSI: ");
+        DEBUG_LOG(rssi);
+        DEBUG_LOG(" Total: ");
+        DEBUG_LOG(contactDuration);
+        DEBUG_LOG("s Close: ");
+        DEBUG_LOG(closeContactDuration);
+        DEBUG_LOG("s Status: ");
+        DEBUG_LOGN(exposureStatus);
+        
+        if (isExposure) {
+            DEBUG_LOG("*** EXPOSURE EVENT DETECTED with ");
+            DEBUG_LOG(deviceAddress);
+            DEBUG_LOG(" (");
+            DEBUG_LOG(closeContactDuration);
+            DEBUG_LOGN(" seconds close contact) ***");
+        }
+        
+        String data = String(currentTime) + "," + deviceAddress + "," + String(rssi) + "," + deviceId + "," + String(lastUploadDuration) + "," + String(contactDuration) + "," + String(closeContactDuration) + "," + exposureStatus;
+        storeData(DATA_FILE, data);
+    }
+
+    void processScannedDevice(BLEAdvertisedDevice device) {
+        if (isContactTracingDevice(device) && device.getRSSI() >= MIN_RSSI) {
+            DEBUG_LOG("Found contact tracing device: ");
+            DEBUG_LOGN(device.getAddress().toString());
+            DEBUG_LOG("RSSI: ");
+            DEBUG_LOGN(device.getRSSI());
+            recordDeviceContact(device);
+        }
+    }
+
+public:
+    BluetoothScanner(unsigned long unixTime, unsigned long uploadDuration = 0)
+        : startTime(unixTime), lastUploadDuration(uploadDuration) {
+        randomSeed(micros());
+        deviceId = generateDeviceId();
+        DEBUG_LOG("Generated Device ID: ");
+        DEBUG_LOGN(deviceId);
+    }
+
+    void initBluetooth() {
+        BLEDevice::init(BLE_DEVICE_NAME);
+        BLEServer *bleServer = BLEDevice::createServer();
+
+        DEBUG_LOGN("LOG: Started Bluetooth Server!!");
+
+        BLEService *bleService = bleServer->createService(SERVICE_UUID);
+        BLECharacteristic *bleCharacteristic = bleService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ);
+        bleCharacteristic->setValue(CHARACTERISTIC_VALUE);
+        bleService->start();
+
+        BLEAdvertising *advertising = BLEDevice::getAdvertising();
+
+        BLEAdvertisementData adData;
+        adData.setManufacturerData(deviceId);
+        advertising->setAdvertisementData(adData);
+        advertising->addServiceUUID(SERVICE_UUID);
+        advertising->setScanResponse(true);
+        advertising->setMinPreferred(0x06);
+        advertising->setMinPreferred(0x12);
+        advertising->start();
+    }
+
+    void performScan() {
+        BLEScan *scanner = BLEDevice::getScan();
+        scanner->setActiveScan(true);
+        scanner->setWindow(99);
+
+        BLEScanResults *results = scanner->start(SCAN_DURATION, false);
+        int deviceCount = results->getCount();
+
+        DEBUG_LOG("Devices Found: ");
+        DEBUG_LOGN(deviceCount);
+        DEBUG_LOGN("Scan Complete!");
+
+        for (int i = 0; i < deviceCount; i++) {
+            BLEAdvertisedDevice device = results->getDevice(i);
+            processScannedDevice(device);
+        }
+
+        scanner->clearResults();
+        updateDeviceId();
+    }
+};
 
 RTC_DATA_ATTR unsigned long bootCount = 0;
 RTC_DATA_ATTR unsigned long lastUploadDuration = 0;
